7d5a2bfc87b5b765b5409660fb7669eab5c4eac4
TheDavid <david.mijdam@hva.nl>
2017-12-21 15:48:52 +0100

    Added: Created a light that switches between two colors.

---
+++ SpecialSnowflake/Assets/Scripts/LightScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour {
+
+    public float wavy;
+    public Color onLightColor;
+    public Color offLightColor;
+
+    bool goToOn;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        wavy = (Mathf.Sin(Time.time) / 2.0f + 0.5f);
+
+        if (wavy >= 0.99f)
+        {
+            goToOn = false;
+        }
+        if (wavy <= 0.01f)
+        {
+            goToOn = true;
+        }
+
+        GetComponent<SpriteRenderer>().color = Color.Lerp(offLightColor, onLightColor, wavy);
+	}
+}
