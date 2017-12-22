9d6c8d5b8002d066fea64236752e3a64e0b3d081
TheDavid <david.mijdam@hva.nl>
2017-12-22 12:12:53 +0100

    Added: Angels follow the snowflake and the lights are randomnized in timing and color.

---
+++ SpecialSnowflake/Assets/Scripts/AngelScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngelScript : MonoBehaviour {
+
+    public GameObject snowflake;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = new Vector2(transform.position.x, snowflake.transform.position.y);
+		
+	}
+}
--- SpecialSnowflake/Assets/Scripts/LightScript.cs
+++ SpecialSnowflake/Assets/Scripts/LightScript.cs
@@ -8,26 +8,21 @@ public class LightScript : MonoBehaviour {
     public Color onLightColor;
     public Color offLightColor;
 
-    bool goToOn;
+    float timeFactor;
 
 	// Use this for initialization
 	void Start () {
+        timeFactor = Random.Range(1.0f, 5.0f);
 
-	}
+        onLightColor.r = Random.Range(0.2f, 1.0f);
+        onLightColor.g = Random.Range(0.2f, 1.0f);
+        onLightColor.b = Random.Range(0.2f, 1.0f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        wavy = (Mathf.Sin(Time.time) / 2.0f + 0.5f);
-
-        if (wavy >= 0.99f)
-        {
-            goToOn = false;
-        }
-        if (wavy <= 0.01f)
-        {
-            goToOn = true;
-        }
+        wavy = (Mathf.Sin(Time.time * timeFactor) / 2.0f + 0.5f);
 
         GetComponent<SpriteRenderer>().color = Color.Lerp(offLightColor, onLightColor, wavy);
 	}
